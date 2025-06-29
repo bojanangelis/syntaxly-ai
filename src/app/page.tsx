7e38888cd30440e69ef230bf448efbaa7bbc7e37
bojanangelis <bojanangjeleski@gmail.com>
2025-06-29 15:27:47 +0200

    testing code rabbit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button"
 
 export default function Home() {
-  return <Button variant="ghost">test</Button>
+  return <Button variant="ghost">test dadaad</Button>
+<div>testtesttt</div>
 }
